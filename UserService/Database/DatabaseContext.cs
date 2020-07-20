0d758b92f1d78a2ac0915df34bcbf6e906d2e3a9
mandy-cheong <mandycheong@hawooo.com>
2020-07-20 16:31:23 +0800

    Add project files.

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=MSI;Initial Catalog=UserService;Integrated Security=True");
+        }
+    }
+}
